21c23d73faa7636bb86a648776f2c2dc9fd518c8
James Fox <James Fox>
2017-07-27 17:11:04 +0100

    adding integration test and launch config for running individual tests

---
+++ test-integration/ID3TagRepository_ReadTags-spec.ts
@@ -0,0 +1,23 @@
+import "mocha";
+import * as chai from "chai";
+
+import ID3TagRepository from "../src/repository/ID3TagRepository";
+
+import TagBag from "../src/model/TagBag";
+
+const expect = chai.expect;
+
+describe("ID3Repository", () => {
+
+    describe("ReadTags", () => {
+
+        it("should succeed", () => {
+
+            let sut = new ID3TagRepository();
+
+            let result = sut.ReadTags("D:\\temp\\02 Drone Strike.mp3");
+
+            expect(result).to.not.be.null;
+        });
+    });
+});
