3f41456438b5cae1e9a22f4d5a8695574e992d0b
James Fox <James Fox>
2017-09-21 17:34:10 +0100

    first ID3TagController-spec test

--- test/ID3TagController-spec.ts
+++ test/ID3TagController-spec.ts
@@ -1,28 +1,46 @@
 import * as chai from "chai";
-import * as express from "express";
 import "mocha";
 import "mocha-sinon";
 import * as sinon from "sinon";
+import { mockReq, mockRes } from "sinon-express-mock";
 
 import ID3TagController from "../src/controller/ID3TagController";
 import IID3TagService from "../src/service/ID3TagService";
 
 import TagBag from "../src/model/TagBag";
-import TagResult from "../src/model/TagResult";
 
 const expect = chai.expect;
 
 describe("ID3TagController", () => {
 
     let id3TagServiceMock: IID3TagService = <IID3TagService>{};
+    let res: any = mockRes();
+
+    beforeEach(() => {
+
+        res.setHeader = sinon.stub();
+        res.send = sinon.stub();
+    });
 
     describe("readTags", () => {
 
-        it("should return tags", (done) => {
+        it("return result when it gets result", () => {
 
+            const request = {
+                params: {
+                    filename: "foo"
+                },
+            }
+
+            let req: any = mockReq(request);
             id3TagServiceMock.ReadTags = sinon.stub().yields(new TagBag());
 
             let sut: ID3TagController = new ID3TagController(id3TagServiceMock);
+            sut.readTags(req, res);
+
+            expect((<sinon.SinonStub>id3TagServiceMock.ReadTags).callCount).to.equal(1);
+            let response: any = (<sinon.SinonStub>res.send).getCall(0).args[0];
+            expect(response.tags).to.not.be.null;
         });
     });
 });
