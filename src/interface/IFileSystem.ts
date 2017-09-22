17e58122f0415f52c06e611237648cc848ce517c
James Fox <James.Fox@hitachiconsulting.com>
2017-09-22 16:12:21 +0100

    adding readFilePromise to IFileSystem

--- src/interface/IFileSystem.ts
+++ src/interface/IFileSystem.ts
@@ -5,6 +5,7 @@ interface IFileSystem {
         callback: (err: NodeJS.ErrnoException, files: string[]) => void): void;
     getDirectoryFilesSync(directory: string): string[];
     readFile(filename: string, callback: (err: NodeJS.ErrnoException, data: Buffer) => void): void;
+    readFilePromise(filename: string): Promise<any>;
 }
 
 export default IFileSystem;
